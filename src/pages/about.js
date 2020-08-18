5d855149a185b5e8a19bfe71ea641084154c3608
edindzabija <edindzabija@gmail.com>
2020-08-18 19:12:15 +0200

    project setup

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const About = () => {
+  return (
+    <div>
+      <Layout>
+        <h2>About Page</h2>
+
+        <p>something something bla bla bla</p>
+      </Layout>
+    </div>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Layout from "../components/Layout"
 
-const IndexPage = () => <h1>Clean Gatsby Template</h1>
-
+const IndexPage = () => (
+  <div>
+    <Layout>
+      <h1>Something</h1>
+      <p>something more</p>
+    </Layout>
+  </div>
+)
 export default IndexPage
