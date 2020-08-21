44fcdb88bf26d6965ce5891730413600b837cd24
edindzabija <edindzabija@gmail.com>
2020-08-21 04:12:52 +0200

    gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,14 +8,11 @@ module.exports = {
     "gatsby-plugin-sass",
     `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'src',
-        path: `${__dirname}/src/`
-      }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
   ],
 }
